45251ce70c96140c5ff217fcae768f2ba5f475eb
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-03-06 23:21:03 -0700

    add validation for password_confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
             presence: true,
             format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true
+  validates :password_confirmation, presence: true
 
   has_secure_password
   has_secure_token :api_key
+
+  before_save { email.try(:downcase!) }
 end
