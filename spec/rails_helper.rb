b27fe4623102c05424d020067068ff0b1ce8b90f
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-03-06 17:49:04 -0700

    add request spec for background endpoint

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,6 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'webmock/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -68,14 +67,17 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
 
-VCR.configure do |config|
-  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
-  config.hook_into :webmock
-  config.filter_sensitive_data('LOCATION_API_KEY') { ENV['LOCATION_API_KEY'] }
-  config.filter_sensitive_data('WEATHER_API_KEY') { ENV['WEATHER_API_KEY'] }
-  config.default_cassette_options = { re_record_interval: 7.days }
-  config.configure_rspec_metadata!
-end
+# require 'webmock/rspec'
+
+# VCR.configure do |config|
+#   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+#   config.hook_into :webmock
+#   config.filter_sensitive_data('LOCATION_API_KEY') { ENV['LOCATION_API_KEY'] }
+#   config.filter_sensitive_data('WEATHER_API_KEY') { ENV['WEATHER_API_KEY'] }
+#   config.filter_sensitive_data('PHOTO_API_KEY') { ENV['PHOTO_API_KEY'] }
+#   config.default_cassette_options = { re_record_interval: 7.days }
+#   config.configure_rspec_metadata!
+# end
 
 def check_hash_structure(hash, key, data_type)
   expect(hash).to have_key(key)
