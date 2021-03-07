2ed74a375967c197df0397a2000d06f437c1b012
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-03-06 21:39:37 -0700

    add tests for validating that requests specify json content-type and accept

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  before_action :validate_headers
+
   rescue_from ArgumentError, with: :render_invalid_parameters
   rescue_from NoMethodError, with: :render_not_found
   rescue_from JSON::ParserError, with: :render_unavailable
@@ -13,5 +15,15 @@ class ApplicationController < ActionController::API
 
   def render_unavailable
     render json: ErrorSerializer.serialize('external API unavailable'), status: :service_unavailable
+  end
+
+  def render_invalid_headers
+    render json: ErrorSerializer.serialize('invalid content type'), status: :bad_request
+  end
+
+  def validate_headers
+    content_type_json = request.content_type == 'application/json'
+    accept_json = request.accept == 'application/json'
+    render_invalid_headers unless content_type_json && accept_json
   end
 end
