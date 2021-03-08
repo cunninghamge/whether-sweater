39275910c39af221732bcf79b6f82789a3b397ce
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-03-08 11:21:46 -0700

    add current weather service and refactor munchies facade

--- app/facades/munchies_facade.rb
+++ app/facades/munchies_facade.rb
@@ -2,19 +2,12 @@ class MunchiesFacade
   class << self
     def munchie(params)
       route = RouteService.call(params[:start], params[:destination])
-      travel_time = route[:formattedTime]
-      restaurant = RestaurantService.call(params[:destination], params[:food], route[:realTime])
-      forecast_info = forecast(params[:destination]).slice(:weather, :main)
-      Munchie.new(params[:destination], travel_time, forecast_info, restaurant)
-    end
-
-    def forecast(destination)
-      response = Faraday.get('https://api.openweathermap.org/data/2.5/weather') do |req|
-        req.params[:q] = destination
-        req.params[:units] = :imperial
-        req.params[:appid] = ENV['WEATHER_API_KEY']
-      end
-      JSON.parse(response.body, symbolize_names: true)
+      restaurant = RestaurantService.call(
+        params[:destination],
+        params[:food],
+        route[:realTime])
+      forecast = CurrentWeatherService.call(params[:destination])
+      Munchie.new(params[:destination], route[:formattedTime], forecast, restaurant)
     end
   end
 end
---
+++ app/services/current_weather_service.rb
@@ -0,0 +1,20 @@
+class CurrentWeatherService
+  class << self
+    def call(destination)
+      response = conn.get do |req|
+        req.params[:q] = destination
+      end
+      body = JSON.parse(response.body, symbolize_names: true)
+      body.slice(:weather, :main)
+    end
+
+    private
+
+    def conn
+      @conn ||= Faraday.new('https://api.openweathermap.org/data/2.5/weather') do |req|
+        req.params[:units] = :imperial
+        req.params[:appid] = ENV['WEATHER_API_KEY']
+      end
+    end
+  end
+end
