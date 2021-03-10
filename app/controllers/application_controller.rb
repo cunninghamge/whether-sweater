6a37ee15ef71b76dd79e38ae4e4609713a0add29
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-03-09 17:49:50 -0700

    add errorable module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,7 @@
 class ApplicationController < ActionController::API
+  include Errorable
+
   rescue_from ArgumentError, with: :render_invalid_parameters
   rescue_from NoMethodError, with: :render_not_found
   rescue_from JSON::ParserError, with: :render_unavailable
-
-  def render_invalid_parameters(error = 'location is required')
-    render json: ErrorSerializer.serialize(error), status: :bad_request
-  end
-
-  def render_not_found
-    render json: ErrorSerializer.serialize('location not found'), status: :not_found
-  end
-
-  def render_unavailable
-    render json: ErrorSerializer.serialize('external API unavailable'), status: :service_unavailable
-  end
-
-  def render_invalid_credentials
-    render json: ErrorSerializer.serialize('Invalid email or password'), status: :unauthorized
-  end
-
-  def reject_query_parameters
-    return if request.query_parameters.blank?
-
-    render_invalid_parameters('user information must not be sent as query parameters')
-  end
 end
---
+++ app/controllers/concerns/errorable.rb
@@ -0,0 +1,26 @@
+module Errorable
+  def render_invalid_parameters(parameter = 'location')
+    error = "#{parameter} is a required parameter"
+    render json: ErrorSerializer.serialize(error), status: :bad_request
+  end
+
+  def render_not_found
+    render json: ErrorSerializer.serialize('location not found'), status: :not_found
+  end
+
+  def render_unavailable
+    render json: ErrorSerializer.serialize('external API unavailable'), status: :service_unavailable
+  end
+
+  def render_invalid_credentials(error)
+    render json: ErrorSerializer.serialize(error), status: :unauthorized
+  end
+
+  def reject_query_parameters
+    return if request.query_parameters.blank?
+
+    render_invalid_parameters(
+      ["data must be sent in the body of the request",
+      "do not send sensitive data as query parameters"])
+  end
+end
